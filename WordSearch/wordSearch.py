97ced0470c11b6bcc0d8a472d870effaaf5e8a20
Richard Yan <richardyan314@foxmail.com>
2018-01-05 00:29:14 -0500

    give an alternative solution by iteration. At least it passes through the tests

--- WordSearch/wordSearch.py
+++ WordSearch/wordSearch.py
@@ -41,10 +41,12 @@ def searchWord(grid, mask, word, row, column):
     return False
 
 
-def wordIsInGrid(grid, word):
+def wordIsInGrid_rec(grid, word):
     """
     search a string in the grid. The string can be constructed from letters of sequentially
     adjencent cell, where "adjancent" cells are those horizontally or vertically neighbouring.
+
+    recursive version.
 
     :param grid: 2D grid with `__getitem__` method on each dimension
     :param word: the (sub)string to search for
@@ -54,11 +56,143 @@ def wordIsInGrid(grid, word):
     # availability of each cell
     mask = [[True for c in range(len(grid[r]))] for r in range(len(grid))]
 
+    # try to begin the word at each cell
     for r in range(len(grid)):
         for c in range(len(grid[r])):
             if searchWord(grid, mask, word, r, c):
                 return True
 
+    return False
+
+
+
+def wordIsInGrid_iter(grid, word):
+    """
+    search a string in the grid. The string can be constructed from letters of sequentially
+    adjencent cell, where "adjancent" cells are those horizontally or vertically neighbouring.
+
+    iterative version.
+
+    :param grid: 2D grid with `__getitem__` method on each dimension
+    :param word: the (sub)string to search for
+    :return: `True` if the string can be found in the grid
+    """
+
+    # try to begin the word at each cell
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+
+            # Initialization
+
+            # availability of each cell
+            mask = [[True for c in range(len(grid[r]))] for r in range(len(grid))]
+
+            # stack of directions we have traveled
+            # used to simulate backtracking
+            # 0: up, 1: right, 2: down, 3: left
+            # also remember our last position
+            # list(tuple(int, tuple(int, int)))
+            #            dir        x    y
+            dirs = []
+
+            # partition the word to two halves: matched and unmatched
+            matched = []
+            unmatched = list(word[::-1])
+            nextChar = ''
+
+
+
+            # begin to search for where possibly the string can begin at
+
+            row = r
+            column = c
+            nextChar = unmatched.pop()
+
+            # print("\tBegin over")
+            # print("\t\tSearching for: " + nextChar + " @(" + str(row) + ", " + str(column) + ")")
+
+            if grid[row][column] == nextChar:
+                mask[row][column] = False
+                matched.append(nextChar)
+                nextChar = unmatched.pop()
+                dirs.append([0, (row, column)])
+
+                # once we found possible position of first charactor
+                # keep matching the rest
+                while True:
+
+                    dir = dirs[-1][0]
+
+                    # if tried all directions and no match
+                    # backtrack
+                    if dir == 4:
+                        dirs.pop()
+                        unmatched.append(matched.pop())
+                        # if we tried all possible directions
+                        # but no match
+                        if len(dirs) == 0:
+                            # print("\t\t\tNot Found")
+                            break
+                        else:
+                            dirs[-1][0] += 1
+                            continue
+
+                    # decide which cell to match next
+                    # based on last position and direction to go from that pos
+                    row, column = dirs[-1][1]
+                    if dir == 0:
+                        row -= 1
+                    elif dir == 1:
+                        column += 1
+                    elif dir == 2:
+                        row += 1
+                    elif dir == 3:
+                        column -= 1
+
+                    # print("\t\tSearching for: " + nextChar + " @(" + str(row) + ", " + str(column) + ") in dir " + str(
+                    #     dirs[-1][0]))
+
+                    # if we are outside of the grid
+                    # backtrack
+                    if not (0 <= row < len(grid) and 0 <= column < len(grid[0])):
+                        # print("\t\t\tOutside")
+                        dirs[-1][0] += 1
+                        continue
+
+                    # if next character matches cell
+                    if mask[row][column]:
+                        # and if the cell is available
+                        if grid[row][column] == nextChar:
+                            # use the cell
+                            mask[row][column] = False
+                        # the cell is unavailable
+                        else:
+                            dirs[-1][0] += 1
+                            # print("\t\t\tNot match")
+                            continue
+                    # if does not match
+                    # backtrack
+                    else:
+                        dirs[-1][0] += 1
+                        # print("\t\t\tUsed")
+                        continue
+
+                    # at this point, we matched one more character
+
+                    matched.append(nextChar)
+
+                    # if no more left, done
+                    if len(unmatched) == 0:
+                        # print("\t\t\tFound")
+                        return True
+                    else:
+                        # otherwise continue with next
+                        dirs.append([0, (row, column)])
+                        nextChar = unmatched.pop()
+                        continue
+
+    # if we are still not able to find a match
+    # there is no match
     return False
 
 
@@ -75,5 +209,6 @@ if __name__ == "__main__":
     testCases += ("ABCESEEDASFCA",)  # too long to fit in the entire grid
 
     for case in testCases:
-        print("\"" + case + "\"" + " is in the grid." if wordIsInGrid(grid, case)
-              else "\"" + case + "\"" + " is not in the grid.")
+        print("Is " + "\"" + case + "\"" + " in the grid?")
+        for methodName, method in (('Recursion', wordIsInGrid_rec), ('Iteration', wordIsInGrid_iter)):
+            print("\t" + methodName + ": " + str(method(grid, case)))
